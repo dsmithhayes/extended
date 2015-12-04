dc701bfc1247ce88902169ba5a12d9582c4e2d20
Dave Smith-Hayes <dsmithhayes@propellerideas.com>
2015-12-04 14:13:13 -0500

    Wrote the prototype for the ControlIterator, maybe some more methods will come.

--- src/Iterator/ControlIterator.php
+++ src/Iterator/ControlIterator.php
@@ -4,5 +4,13 @@ namespace Extended\Iterator;
 
 interface ControlIterator extends \Iterator
 {
+    /**
+     * @return bool True if on the first iteration
+     */
+    public abstract function first();
 
+    /**
+     * @return bool True if on the last iteration
+     */
+    public abstract function last();
 }
