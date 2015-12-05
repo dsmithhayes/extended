d21ec556f3f5a0447f9d354fe3f68983de5e7888
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-12-05 13:58:14 -0500

    ControlIterator is done.

--- tests/ControlIteratorTest.php
+++ tests/ControlIteratorTest.php
@@ -4,11 +4,15 @@ use Extended\LinkedList;
 
 class ControlerIteratorTest extends PHPUnit_Framework_TestCase
 {
+    protected $first = 'first';
+    protected $last  = 'last';
+    protected $exampleSet = ['first', 'second'];
+
     public function testFirstMethod()
     {
         $list = new LinkedList();
 
-        $list->add(1);
+        $list->add($this->first);
 
         for ($list->rewind(); $list->valid(); $list->next()) {
             $this->assertTrue($list->first());
@@ -23,10 +27,10 @@ class ControlerIteratorTest extends PHPUnit_Framework_TestCase
      */
     public function testLastMethodTrue($list)
     {
-        $list->add(2);
+        $list->add($this->last);
 
         for ($list->rewind(); $list->valid(); $list->next()) {
-            if ($list->current() === 2) {
+            if ($list->current() === $this->last) {
                 $this->assertTrue($list->last());
                 break;
             }
@@ -41,10 +45,8 @@ class ControlerIteratorTest extends PHPUnit_Framework_TestCase
     public function testLastMethodFalse($list)
     {
         for ($list->rewind(); $list->valid(); $list->next()) {
-            if ($list->current() == 1) {
-                $this->assertFalse($list->last());
-                break;
-            }
+            $this->assertFalse($list->last());
+            break;
         }
 
         return $list;
@@ -64,16 +66,27 @@ class ControlerIteratorTest extends PHPUnit_Framework_TestCase
         return $list;
     }
 
+    /**
+     * @depends testForeachTrue
+     */
+    public function testForeachFalse($list)
+    {
+        foreach ($list as $l) {
+            $this->assertFalse($list->last());
+            break;
+        }
+
+        return $list;
+    }
+
+
     public function testArrayConstruction()
     {
-        $first = 'first';
-        $last  = 'last';
-
-        $list = new LinkedList([$first, $last]);
+        $list = new LinkedList($this->exampleSet);
 
         foreach ($list as $l) {
             if ($list->first()) {
-                $this->assertEquals($first, $l);
+                $this->assertEquals($this->first, $l);
                 break;
             }
         }
