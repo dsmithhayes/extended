652457bfcbd24da52a54750a44fe0cfae5912c00
Dave Smith-Hayes <dsmithhayes@propellerideas.com>
2015-12-29 17:53:10 -0500

    What am I doing with that mutator?

--- src/Mutator/Mutator.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Extended\Mutator;
-
-/**
- * Combines the two other mutator interfaces in a single, easy to access
- * interface.
- */
-interface Mutator extends Getter, Setter { }
