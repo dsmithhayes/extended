d70e1497bc72202e0748ebc2541033d1a4cf290d
Dave Smith-Hayes <dsmithhayes@propellerideas.com>
2015-12-31 11:29:59 -0500

    I wrote the iteration test realizing that a stack should iterate FILO. My bad.

--- tests/AdvancedStackTestCase.php
+++ tests/AdvancedStackTestCase.php
@@ -8,5 +8,14 @@ class AdvancedStackTestCase extends PHPUnit_Framework_TestCase
     {
         $stack = new AdvancedStack([0, 1, 2, 3]);
         $this->assertEquals(3, $stack->key());
+        return $stack;
+    }
+
+    /**
+     * @depends testAdvancedStackPointer
+     */
+    public function testIteration($stack)
+    {
+
     }
 }
