8337927d8008ff9457d9767f8d11eaf8b685d3f3
Dave Smith-Hayes <dsmithhayes@gmail.com>
2016-04-19 22:43:48 -0400

    Making the composer requirements just fine.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,9 @@ apt-get auto-remove -y
 apt-get install -y php7.0 \
                    php7.0-fpm \
                    php7.0-cli \
-                   php7.0-xml
+                   php7.0-xml \
+                   php7.0-zip \
+                   php7.0-bz2
 
 # add composer
 echo "Downloading Composer..."
