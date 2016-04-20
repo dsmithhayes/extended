96f1d3fb17cec21d7d25754c3e4b968d9a1b731d
Dave Smith-Hayes <dsmithhayes@gmail.com>
2016-04-19 22:37:49 -0400

    Adding documentation, running provisioning tests.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+# add the PHP7 repositories
+echo "Adding PHP7 repositories..."
+add-apt-repository -y ppa:ondrej/php
+
+# update the system, install php7
+echo "Updating the box's system..."
 apt-get update
 apt-get upgrade -y
-
-# add the PHP7 repositories
+apt-get auto-remove -y
+apt-get install -y php7.0 \
+                   php7.0-fpm \
+                   php7.0-cli \
+                   php7.0-xml
 
 # add composer
-    # update composer
+echo "Downloading Composer..."
+curl -sS http://getcomposer.org/installer | php --
 
-# run phpunit
+echo "Moving composer to a PATH directory..."
+chmod a+x composer.phar
+mv composer.phar /usr/local/bin/composer
--- readme.md
+++ readme.md
@@ -1,4 +1,22 @@
 # Exnteded
 
 Classes and interfaces that expand some concepts from the SPL that I find I
-use across many projects.
+use across many projects. Basic data structures and their usages are implemented
+throughout this library.
+
+## Testing
+
+There are two ways to test the Extended package.
+
+### Locally
+
+    $ composer install
+    $ ./vendor/bin/phpunit
+
+### Virtually
+
+    local$ vagrant up --provision
+    local$ vagrant ssh
+    vagrant$ cd /vagrant
+    vagrant$ composer install
+    vagrant$ ./vendor/bin/phpunit
