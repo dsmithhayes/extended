0cb4599d65645b1ce88c4103572ec34911d207e2
Dave Smith-Hayes <dsmithhayes@gmail.com>
2016-11-01 21:17:51 -0400

    Added a mappable interface.

---
+++ src/Collections/Mappable.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * When a class implements Mappable, they are accepting a callback to apply to
+ * each member of an array the object has access to.
+ */
+interface Mappable
+{
+    /**
+     * @param callable $callback
+     *      The callback must return a value that will replace the value in
+     *      the array.
+     */
+    public function map(callable $callback);
+}
